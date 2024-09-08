24719a2102b326d383dc132dc3500003deb24f8b
IsraelMrakpor <mraisrael05@gmail.com>
2024-09-08 13:44:05 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
    SESSION_ID: process.env.SESSION_ID || "Session~7inQCuhh",
    HANDLERS: process.env.HANDLER === "false" || process.env.HANDLER === "null" ? "^" : "[.]",
    BOT_INFO: process.env.BOT_INFO || "ᴀsᴛʀᴏ;ғxᴏᴘ-ᴍᴅ", //YourName;BotName;Image/VideoLink
-   SUDO: process.env.SUDO || "2348039607375,2349162411207,4915252819677",
+   SUDO: process.env.SUDO || "2349039982227",
    ANTICALL: process.env.ANTICALL || false,
    PPRESENCE: process.env.PRESENCE || "available", // "unavailable", "available", "composing", "recording", "paused"
    TIME_ZONE: process.env.TIME_ZONE || "Africa/Lagos",
